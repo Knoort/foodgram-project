6226de5f166b2be7a634fe2ec94f1ff34f5ea746
Knoort <m-kris@narod.ru>
2021-06-20 19:10:38 +0300

    Последняя работавшая версия+поменял имена переменных POSTGRES_ на DB_. Поменял собственника каталога dev на сервере

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
         # 'NAME': BASE_DIR / 'db_from_postgres.sqlite3',
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     }
