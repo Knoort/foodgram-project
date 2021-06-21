247198e3134f91a8bbdcc30dc74599bab8a0a430
Knoort <m-kris@narod.ru>
2021-06-21 15:52:41 +0300

    Вернул #noqa, но исключил foodgram/urls.py из проверки

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls import handler404, handler500  # noqa
+from django.conf.urls import handler404, handler500  #noqa
 from django.conf.urls.static import static
 
-handler404 = 'recipes.views.page_not_found'   # noqa
-handler500 = 'recipes.views.server_error'     # noqa
+handler404 = 'recipes.views.page_not_found'   #noqa
+handler500 = 'recipes.views.server_error'     #noqa
 
 urlpatterns = [
     #  регистрация и авторизация
