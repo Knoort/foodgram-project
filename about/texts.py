58774c4e2591be03ddb31d56e678f0fa91547173
Knoort <m-kris@narod.ru>
2021-06-24 20:20:36 +0300

    Сделал страницы about, поправил стили кастом текстов (формы сброса пароля, пустая страница закупок, страницы about. Поправил пажинатор - стрелки.

--- about/texts.py
+++ about/texts.py
@@ -1,6 +1,39 @@
+ABOUT_AUTHOR_HEADER = 'Дмитрий Струнин'
+
 ABOUT_AUTHOR = """
-Здесь будет раздел "Обо мне"
+-   Разработка бэкенда сайтов
+-   Разработка программ для
+    мироконтроллеров
+-   Системы управления электроприводом
+
+github:     @Knoort
+Email:      m-kris@narod.ru
 """
+
+ABOUT_TECH_HEADER = 'ТЕхнологии, используемые для этого сайта:'
+
+ABOUT_TECH_TABLE = (
+    {
+        'name': 'Python 3',
+        'site': 'https://www.python.org'
+    },
+    {
+        'name': 'Django 3.1',
+        'site': 'https://www.djangoproject.com'
+    },
+    {
+        'name': 'Django REST framework',
+        'site': 'https://www.django-rest-framework.org'
+    },
+    {
+        'name': 'PostgreSQL',
+        'site': 'https://www.postgresql.org'
+    },
+    {
+        'name': 'Docker',
+        'site': 'https://www.docker.com'
+    },
+)
+
 ABOUT_TECH = """
-Здесь будет описание программных средств
 """
--- about/views.py
+++ about/views.py
@@ -1,6 +1,12 @@
 from django.views.generic.base import TemplateView
 
-from .texts import ABOUT_AUTHOR, ABOUT_TECH
+from .texts import (
+    ABOUT_AUTHOR_HEADER,
+    ABOUT_AUTHOR,
+    ABOUT_TECH_HEADER,
+    ABOUT_TECH_TABLE,
+    ABOUT_TECH
+)
 
 
 class AboutView(TemplateView):
@@ -11,9 +17,12 @@ class AboutView(TemplateView):
         context = super().get_context_data(**kwargs)
         if self.page == 'author':
             context['about_title'] = 'Об авторе'
+            context['about_header_title'] = ABOUT_AUTHOR_HEADER
             context['about_text'] = ABOUT_AUTHOR
         elif self.page == 'tech':
             context['about_title'] = 'Технологии'
+            context['about_header_title'] = ABOUT_TECH_HEADER
+            context['about_table'] = ABOUT_TECH_TABLE
             context['about_text'] = ABOUT_TECH
 
         return context
